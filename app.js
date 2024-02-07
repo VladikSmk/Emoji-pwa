ffa0506c9ca6cab931b9a85af1f5b4d42acf16f1
VladikSmk <152417644+VladikSmk@users.noreply.github.com>
2024-02-07 20:26:07 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+class Emoji
+{
+    constructor(food){
+        this.food = food;
+    }
+
+    feed(){
+        this.food += 10;
+        if(this.food >= 100){
+            this.food = 100;
+        }
+        
+    };
+    
+    checkState(){
+        if(emoji.food > 50){
+            document.querySelector('.pet_img').src = 'vecteezy_emoji-feel-good-smile-happy-png-file_10313700.png'
+        }else {
+            document.querySelector('.pet_img').src = 'Sad-Emoji-PNG-Free-Download.png'
+        };
+        document.querySelector('.food').textContent = emoji.food;
+
+    }
+}
+
+
+const emoji = new Emoji(100);
+
+
+
+let intervalId = setInterval(() => {
+    emoji.food -= Math.floor(Math.random() * 20);
+    if(emoji.food <=0){
+        emoji.food = 0;
+        clearInterval(intervalId);
+    }
+    emoji.checkState();
+
+}, 1000);
+
+let feed = document.querySelector('.feed');
+
+feed.addEventListener('click', function(){
+    emoji.feed();
+    
+})
+
+
